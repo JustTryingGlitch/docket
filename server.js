87849869798150d4f18480ab19508608b2446ce9
Parwinner <parwinner2@gmail.com>
2021-10-09 21:39:41 +1000

    Cosmetic changes etc
    Adds comments to server.js
    Changes title of index.html to "Docket"
    Updates stylesheet with a border between messages (theoretically)

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
+//filepath to the files that will be public to every user
 const public_dir = path.join(__dirname, '/public');
 
 app.use(express.static(public_dir));
+//uses a socket server to send messages
 
 io.on("connection", (client) => {
     console.log("a user has connected")
@@ -20,5 +22,5 @@ io.on("connection", (client) => {
     });
     client.on("disconnect", () => console.log("a user has disconnected"))
 });
-
+//informs the http server app that it needs to receive information from a specific port 
 server.listen(port, console.log(`Listening on: http://localhost:${port}`))
